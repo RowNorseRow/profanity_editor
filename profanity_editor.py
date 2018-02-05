d85a4b5e10ab3842b93a004fb721d86f61f99112
Allen Ellis <diamondstateellis@gmail.com>
2018-02-04 22:26:29 -0600

    Step 1: Read the text from a file in the directory

---
+++ profanity_editor.py
@@ -0,0 +1,10 @@
+def read_text():
+    quotes = open("C:\Users\Owner\Documents\movie quotes\movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
+    quotes.close()
+
+read_text()
+
+    
+    
