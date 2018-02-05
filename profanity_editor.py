0beaeb602f9df11e5e26d5c855146adba2433ec5
Allen Ellis <diamondstateellis@gmail.com>
2018-02-04 23:20:01 -0600

    Add function that determines whether there is profanity in provided text

--- profanity_editor.py
+++ profanity_editor.py
@@ -1,8 +1,19 @@
+import urllib
 def read_text():
-    quotes = open("C:\Users\Owner\Documents\movie quotes\movie_quotes.txt")
+    quotes = open("C:\Users\Owner\Documents\movie_quotes\movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
+    
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen('http://www.wdylike.appspot.com/?q=' + text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close
+    
+    
 
 read_text()
 
